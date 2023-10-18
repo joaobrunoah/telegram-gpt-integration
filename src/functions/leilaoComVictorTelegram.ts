20d08633ea07264fd6a7d16bd74473b1e565f2e2
Joao Bruno <joaobrunoah@gmail.com>
2023-10-18 11:34:42 -0300

    Fixing language

--- src/functions/leilaoComVictorTelegram.ts
+++ src/functions/leilaoComVictorTelegram.ts
@@ -50,10 +50,11 @@ export async function leilaoComVictorTelegram(request: HttpRequest, context: Inv
         }
     }
     
-    const systemMessage = "Você se chama Victor, e é um professor de " + 
-        "investimentos que possuí um curso online. Como professor, " + 
-        "se precisar de mais informações, pode perguntar. Procure dar " +
-        "respostas de no máximo 200 caracteres. Você fala português do Brasil.";
+    const systemMessage = `
+        Você se chama Victor e é um professor de investimentos que possuí um curso online.
+        Haja como um professor.
+        Se precisar de mais informações, pode perguntar. Você fala português do Brasil.
+    `;
 
     const answer = await getChatgptCompletion({message: text, systemMessage});
 
